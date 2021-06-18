bff13bc3cc06adaf1066e94e918378eb0e60fbf7
Stilyan <sbaklarev@gmail.com>
2021-06-18 19:25:29 +0300

    I add two solving-problmes from Stacks and Queue exercise

---
+++ StascksAndQueues-Exercise/BasicQueueOperations/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BasicQueueOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] NSX = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            int n = NSX[0];
+            int s = NSX[1];
+            int x = NSX[2];
+            int[] allElements = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            Queue<int> myQueue = new Queue<int>(allElements);
+          
+
+            for (int i = 0; i < s; i++)
+            {
+                myQueue.Dequeue();
+            }
+
+            if (myQueue.Contains(x))
+            {
+                Console.WriteLine("true");
+            }
+            else if (myQueue.Count == 0)
+            {
+                Console.WriteLine(0);
+            }
+            else
+            {
+                int minNum = myQueue.Min();
+                Console.WriteLine(minNum);
+            }
+            
+        }
+    }
+}
---
+++ StascksAndQueues-Exercise/BasicStackOperations/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BasicStackOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] NSX = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int n = NSX[0];
+            int s = NSX[1];
+            int x = NSX[2];
+
+            int[] elements = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            Stack<int> sequence = new Stack<int>(elements);
+
+            for (int i = 0; i < s; i++)
+            {
+                sequence.Pop();
+            }
+
+            if (sequence.Contains(x))
+            {
+                Console.WriteLine("true");
+            }
+            
+            else if (sequence.Count == 0)
+            {
+                Console.WriteLine(0);
+            }
+            else
+            {
+                Console.WriteLine(sequence.Pop());
+            }
+        }
+    }
+}
