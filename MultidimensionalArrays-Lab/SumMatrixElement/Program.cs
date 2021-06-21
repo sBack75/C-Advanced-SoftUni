b2a047027d73ee610548e9e6059affe64c880b9f
Stilyan <sbaklarev@gmail.com>
2021-06-21 22:54:35 +0300

    I add only one exercise from MultidimensionalArr

---
+++ MultidimensionalArrays-Lab/SumMatrixElement/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+
+namespace SumMatrixElement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] command = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            int rows = command[0];
+            int col = command[1];
+            int[,] myFirstMultiArr = new int[rows, col];
+
+            
+            int sum = 0;
+            for (int row = 0; row < rows; row++)
+            {
+                int[] arr = Console.ReadLine()
+                   .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                   .Select(int.Parse)
+                   .ToArray();
+                for (int colomn = 0; colomn < col; colomn++)
+                {
+
+                    myFirstMultiArr[row, colomn] = arr[colomn];
+                   
+                }
+            }
+
+            foreach (var item in myFirstMultiArr)
+            {
+                sum += item;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
