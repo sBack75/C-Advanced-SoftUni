c582d4a404d4f56fcaba54545c63d542545ed0b6
Stilyan <sbaklarev@gmail.com>
2021-06-29 23:05:14 +0300

    I add problem solving

---
+++ SetsAndDictionarity-lab/AverageStudentGrades/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AverageStudentGrades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberStudents = int.Parse(Console.ReadLine());
+            Dictionary<string, List<double>> students = new Dictionary<string, List<double>>();
+
+            for (int i = 0; i < numberStudents; i++)
+            {
+                string[] curData = Console.ReadLine()
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+
+                string name = curData[0];
+                double grades = double.Parse(curData[1]);
+                List<double> mySecList = new List<double>();
+                mySecList.Add(grades);
+                if (students.ContainsKey(name) == true)
+                {
+
+                  
+                    students[name].Add(grades);
+
+                }
+                else
+                {
+                    students.Add(name, mySecList);
+                }
+            }
+
+            foreach (var item in students)
+            {
+                Console.WriteLine($"{item.Key} {item.Value}");
+
+               
+            }
+        }
+    }
+}
---
+++ SetsAndDictionarity-lab/CountSameValueInArray/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CountSameValueInArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<double, int> counter = new Dictionary<double, int>();
+
+            double[] numbers = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(double.Parse)
+                .ToArray();
+
+            
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (counter.ContainsKey(numbers[i]) == false)
+                {
+                    counter.Add(numbers[i],0);
+                }
+
+                counter[numbers[i]]++;
+                 
+            }
+            foreach (var item in counter)
+            {
+                Console.WriteLine($"{item.Key} - {item.Value} times");
+            }
+        }
+    }
+}
